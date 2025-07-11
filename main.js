44a0a91917dcb871a4cc4540abb2ae5fe29f7ec1
erikagiella <erikagiella93@gmail.com>
2025-07-11 23:56:21 +0200

    correzioni

--- main.js
+++ main.js
@@ -46,16 +46,17 @@ for (let i = 1; i <= 100; i++) {
     resto_5 = i % 5
 
     //SE i è divisibile per 3 e non per 5
-    if (resto_3 === 0 && resto_5 != 0) {
+
+    if (resto_3 === 0 && resto_5 === 0) {
+        message = `${fizz}${buzz}`
+    }
+    //ALTRIMENTI SE i è divisibile per 3 
+    else if (resto_3 === 0) {
         message = fizz
     }
-    //ALTRIMENTI SE i è divisibile per 5 e non per 3
-    else if (resto_3 != 0 && resto_5 === 0) {
+    //ALTRIMENTI SE i è divisibile per 5
+    else if (resto_5 === 0) {
         message = buzz
-    }
-    //ALTRIMENTI SE i è divisibile per 3 e per 5
-    else if (resto_3 === 0 && resto_5 === 0) {
-        message = `${fizz}${buzz}`
     }
     //ALTRIMENTI se non è divisibile nè per 3 nè per 5
     else {
