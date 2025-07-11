28ef45dc46f8e89575d99ff05a5b52f70707d4c3
erikagiella <erikagiella93@gmail.com>
2025-07-11 23:16:00 +0200

    inseriti if/else if/else per valutzione casistiche + stampa del messaggio

--- main.js
+++ main.js
@@ -20,12 +20,11 @@
 
 Preparazione:
 - creo due costanti Fizz e Buzz
-- creo variabili "resto_3" "resto_5" "resto_tot" "message"
+- creo variabili "resto_3" "resto_5" "message"
 - creo un ciclo for che incrementi la variabile i da 1 a 100 (incluso)
     - nelle due variabili "resto_3" e "resto_5" calcolo il resto della divisione di i per 3 e per 5
-    - sommo in un'altra variabile i due resti: se il numero è divisibile sia per 3 che per 5 questa variabile conterrà uno 0
     - utilizzo un if/else if per verificare in quale delle varie casistiche rientro
-    - stampo a seconda della casisticanb m
+    - stampo messaggio a seconda della casistica
 */
 
 
@@ -37,12 +36,34 @@ const buzz = 'Buzz'
 let message = ''
 let resto_3
 let resto_5
-let resto_tot
+
 
 //ciclo for 1-100
 for (let i = 1; i <= 100; i++) {
+
+    //calcolo se ci sono resti per le divisioni di i per 3 e 5
     resto_3 = i % 3
     resto_5 = i % 5
-    resto_tot = resto_3 + resto_5
+
+    //SE i è divisibile per 3 e non per 5
+    if (resto_3 === 0 && resto_5 != 0) {
+        message = fizz
+    }
+    //ALTRIMENTI SE i è divisibile per 5 e non per 3
+    else if (resto_3 != 0 && resto_5 === 0) {
+        message = buzz
+    }
+    //ALTRIMENTI SE i è divisibile per 3 e per 5
+    else if (resto_3 === 0 && resto_5 === 0) {
+        message = `${fizz}${buzz}`
+    }
+    //ALTRIMENTI se non è divisibile nè per 3 nè per 5
+    else {
+        message = i
+    }
+
+    //stampo il risultato sia in console che sulla pagina
+    console.log(message)
+    document.writeln(message)
 }
 
